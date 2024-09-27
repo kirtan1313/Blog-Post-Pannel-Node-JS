29254ff65cc3dd253935922d614d604a966d8184
kirtan1313 <mkirtan013@gmail.com>
2024-09-27 15:35:27 +0530

    Blog Post Pannel

---
+++ Confring/dataBase.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://mkirtan013:KvJNO0YeJrikMEFt@cluster0.egcsg.mongodb.net/blog-post')
+  .then(() => console.log('Data base is Connected Successfully....')).catch((err)=>{
+    console.log('Database conectivity is faild..!',err);
+  });
