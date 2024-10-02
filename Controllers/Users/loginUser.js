6e5e561248b80684a7c21c78b9eefa54b3e66ac7
kirtan1313 <mkirtan013@gmail.com>
2024-10-02 11:39:40 +0530

    ChangePassUpdate

--- Controllers/Users/loginUser.js
+++ Controllers/Users/loginUser.js
@@ -1,19 +1,53 @@
 const authModule = require('../../Modules/Authontication')
+const bcrypt = require('bcrypt');
 
 
 
-const userLoginFormController = (req , res) =>{
-    res.render('login')
+const userLoginFormController = (req, res) => {
+  res.render('login')
 }
 
-const userLogoutController =async (req , res) =>{
-  req.logout((err)=>{
-    if(err){
+const userLogoutController = async (req, res) => {
+  req.logout((err) => {
+    if (err) {
       done(err)
-    }else{
+    } else {
       res.redirect('/login')
     }
   })
 }
 
-module.exports = {userLoginFormController  , userLogoutController}
+const changePassword = (req, res) => {
+
+  res.render('changePasss');
+}
+
+const updatePassword = (req, res) => {
+  const { password } = req.user;
+  const { curr_Pass, new_Pass, conf_Pass } = req.body;
+
+  bcrypt.compare(curr_Pass, password, (err, result) => {
+    if (result) {
+      console.log('pass Match');
+        if(new_Pass == conf_Pass){
+          bcrypt.hash(new_Pass, 10, async (err, hashPass) => {
+            console.log('hashhh',hashPass);
+            if(err){
+              res.redirect('/changePasss')
+            }
+
+            const upadtePass = await authModule.updateOne({_id:req.user._id},{password:hashPass})
+            console.log('upadtepasss',upadtePass);
+            if(upadtePass){
+              res.redirect('/login')
+            }
+          })
+        }
+      } else {
+      console.log('pass Not Match');
+    }
+  })
+
+}
+
+module.exports = { userLoginFormController, userLogoutController, changePassword, updatePassword }
--- Routres/Routers.js
+++ Routres/Routers.js
@@ -9,12 +9,8 @@ const passport = require('../Confring/passport_confi')
 const isAuth = require('../meddlewere/midlewereAuth')
 
 
-//Routing area
-
-//Home router
 router.get('/',isAuth,controller.defaultController);
 
-//bloging add
 router.get('/addForm',isAuth, BlogController.addBlogFormController)
 router.post('/addblog',isAuth, upload.single('image'), BlogController.createBlogController)
 router.get('/allblog',isAuth, BlogController.getBlogController)
@@ -23,14 +19,15 @@ router.get('/edit/:id',isAuth,BlogController.editBlogController)
 router.post('/updateBlogs/:id',isAuth, upload.single('updateimg'),BlogController.updateBlogController)
 router.get ('/delete/:id',isAuth, BlogController.deleteBlogController)
 
-//singup page 
 router.get('/signup',registerController.userRegFormController)
 router.post('/signuppost',registerController.userRegFormPostController)
 
-//login page
 router.get('/login',loginController.userLoginFormController)
 router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }))
 router.get('/logout' ,loginController.userLogoutController)
+
+router.get('/changepass',isAuth,loginController.changePassword)
+router.post('/upadetPass',loginController.updatePassword)
 
 
 module.exports = router;
